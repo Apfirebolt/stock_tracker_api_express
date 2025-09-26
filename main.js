b8d8e5dcc70d2da1e401ea465525ebcd675bf5c0
Apfirebolt <aspper20@gmail.com>
2025-09-27 02:29:54 +0530

    feat: added auth

---
+++ main.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+
+import authRoutes from './routes/auth.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:8080'
+}))
+
+const __dirname = path.resolve()
+
+app.use('/api/auth', authRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
+
+export default app
